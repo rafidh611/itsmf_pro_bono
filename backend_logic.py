020ad4f80429d160c50d8913b7bf7e8f49bd5132
AMERICAS/Rafid_Hasan <Rafid.Hasan@Dell.com>
2025-04-09 05:29:31 -0500

    more changes

--- backend_logic.py
+++ backend_logic.py
@@ -1,9 +1,9 @@
 import chromadb
 from openai import OpenAI
 
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 #chromdb client
 client = chromadb.PersistentClient("./collection")
