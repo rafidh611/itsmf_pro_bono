d22fdc58f1116e8a61f2d581f49905a88d579816
AMERICAS/Rafid_Hasan <Rafid.Hasan@Dell.com>
2025-04-09 17:14:02 -0500

    Update streamlit_ui.py

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -6,13 +6,15 @@ if 'messages' not in st.session_state:
 
 
 def display_messages():
+    print("display_messages")
     for message in st.session_state['messages']:
         with st.chat_message(message['role']):
             st.write(message['content'])
 
 def run_user_query(query):
+    print("run_user_query")
     response = run_rag_llm(query)
-    st.session_state['messages'].append({'role': 'user', 'content': user_question})
+    st.session_state['messages'].append({'role': 'user', 'content': query})
     st.session_state['messages'].append({'role': 'assistant', 'content': response})
     display_messages()
 
