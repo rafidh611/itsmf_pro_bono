be215dd28de5cc67629156e2f9389a45cd59a4e3
AMERICAS/Rafid_Hasan <Rafid.Hasan@Dell.com>
2025-04-09 00:58:00 -0500

    First commit

---
+++ streamlit_ui.py
@@ -0,0 +1,55 @@
+import streamlit as st
+
+
+
+st.title(":blue[ITSMF Chat Bot]")
+
+
+st.markdown(
+    """
+    <style>
+    [data-testid="stAppViewContainer"] {
+        background-color: #222222;
+    }
+
+    [data-testid="stSidebar"] {
+        background-color: black;
+    }
+
+    [data-testid="stHeader"] {
+        background-color: black;
+    }
+
+    [data-testid="stBottomBlockContainer"] {
+        background-color: black;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+
+if st.sidebar.button(':red[Reset Chat]'):
+    pass
+
+st.sidebar.write(" ")
+st.sidebar.write(" ")
+st.sidebar.write(" ")
+
+if st.sidebar.button(":blue[What is ITSMF?]"):
+    pass
+
+if st.sidebar.button(":blue[How do I join ITSMF?]"):
+    pass
+
+
+if st.sidebar.button(":blue[Is ITSMF hiring?]"):
+    pass
+
+
+user_question = st.chat_input("Ask a question...")
+
+
+if user_question:
+    pass
