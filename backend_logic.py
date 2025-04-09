8c241f0753a10ec278f0d69108c1122253b4cf6f
AMERICAS/Rafid_Hasan <Rafid.Hasan@Dell.com>
2025-04-09 12:42:18 -0500

    Added RAG with OpenAI

--- backend_logic.py
+++ backend_logic.py
@@ -20,11 +20,38 @@ openai_client = OpenAI()
 
 
 def run_user_query(query):
+
+    rag_results = collection.query(query_texts=[query], n_results=3, include=["documents", "metadatas"])
+
+    print("rag_results", rag_results)
+
+
+    search_results = []
+
+    for res in rag_results["documents"]:
+        for doc, meta in zip(res, rag_results["metadatas"][0]):
+            # Format the document text and its metadata
+            metadata_str = ", ".join(f"{key}: {value}" for key, value in meta.items())
+            search_results.append(f"{doc}\nMetadata: {metadata_str}")
+    search_text = "\n\n".join(search_results)
+
+
+    print("search_text", search_text)
+
+
+    prompt = f"""Your task is to answer the following user question using the supplied search results.
+        User Question: {query}
+        Search Results: {search_text}
+        """
+
+
+
+
     response = openai_client.chat.completions.create(
         model='gpt-4o',
         messages=[
            {"role": "system", "content": "You're a helpful assistant who looks up specific information about an organization called Information Technology Senior Management Forum (ITSMF) from user queries. If you are not able to answer the question, say 'I'm sorry, I don't have access to that information.'"},
-           {"role": "user", "content": query},
+           {"role": "user", "content": prompt},
         ]
     )
 
